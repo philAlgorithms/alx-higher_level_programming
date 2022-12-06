9595b1e4db69d7d1c979612b4c5d71c8dda2711b
Philip Nwokedi <philippos764@gmail.com>
2022-12-05 19:44:35 -0800

    task 4 attempt 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """retrieves an element from a list like in C without modifying the original"""
+    """retrieves an element from a list like in C
+    without modifying the original"""
     copy_list = my_list[:]
     if idx < 0:
         return copy_list
