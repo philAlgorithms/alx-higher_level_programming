9a2f64a4411efde015012debe33d61b1665af067
Philip Nwokedi <philippos764@gmail.com>
2022-12-19 20:11:01 -0800

    Advanced Task 2 retry

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -18,4 +18,4 @@ def safe_function(fct, *args):
         return (res)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    return (None)
