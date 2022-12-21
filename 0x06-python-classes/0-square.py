f1b9ea0d120d12213681d769d98ec69cf02afc17
Philip Nwokedi <philippos764@gmail.com>
2022-12-20 19:34:09 -0800

    Task 2

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,7 @@
 
 """Defines a square class"""
 
+
 class Square:
     """Represents a square class"""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a square class"""
+
+
+class Square:
+    """Represents a square class"""
+
+    def __init__(self, size):
+        """Initializes a new Square instance
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
